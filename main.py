fdd9e80bcb59b09e15eef4259df07fa0cc6d7508
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-06-11 21:56:09 -0300

    Criando nosso site e colocando no ar

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 from flask import Flask
+
+app = Flask(__name__)#nome do projeto
+
+#criar servidor local/rota/homepage
+@app.route("/")
+def homepage():
+    return "FakePinterest - Meu primeiro site no ar!"
+
+#colocar site no ar
+if __name__ == "__main__":
+      app.run(debug=True) #qualuqer alteração que faça , atualiza
+
+
