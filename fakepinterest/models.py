895a9995f73f410bcf8d889a8d6b62756a8324b4
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-06-15 21:01:12 -0300

    Implementar funcionalidades de Login e Criar Conta

--- fakepinterest/models.py
+++ fakepinterest/models.py
@@ -1,6 +1,6 @@
 #criar a estrutura do banco de dados
 from fakepinterest import database, login_manager
-from datetime import datetime
+from datetime import datetime, timezone
 from flask_login import UserMixin
 
 #funcao que seleciona um usuario----------
@@ -12,15 +12,15 @@ def load_usuario(id_usuario):
 #criar tabelas-------------
 class Usuario(database.Model, UserMixin):
     id = database.Column(database.Integer, primary_key=True)
-    username = database.Column(database.String, nullable=True)
-    email = database.Column(database.String, nullable=True, unique=True)
-    senha = database.Column(database.String, nullable=True)
-    fotos = database.Relationship("Foto", backref="usuario", lazy=True)
+    username = database.Column(database.String, nullable=False)
+    email = database.Column(database.String, nullable=False, unique=True)
+    senha = database.Column(database.String, nullable=False)
+    fotos = database.relationship("Foto", backref="usuario", lazy=True)
 
 
 #foto precisa relacionar/ usuario(id_usuario)
 class Foto(database.Model):
     id = database.Column(database.Integer, primary_key=True)
     imagem = database.Column(database.String, default="default.png")
-    data_criacao = database.Column(database.DateTime, nullable=False, default=datetime.utcnow())
+    data_criacao = database.Column(database.DateTime, nullable=False, default=lambda:datetime.now(datetime.utc))
     id_usuario = database.Column(database.Integer, database.ForeignKey('usuario.id'), nullable=False)
--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -1,21 +1,31 @@
 #criar as rotas do nossso site (os links)
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect
 #import o app dentro do init
-from fakepinterest import app
-from flask_login import login_required
+from fakepinterest import app, database, bcrypt
+from fakepinterest.models import Usuario, Foto
+from flask_login import login_required, login_user, logout_user
 from fakepinterest.forms import FormLogin, FormCriarConta
 
 #criar tela login-------
 @app.route("/", methods=["GET", "POST"])
 def homepage():
-    formlogin = FormLogin()                 #form = referencia para homepage
-    return render_template("homepage.html", form=formlogin)
+    form_login = FormLogin()                 #form = referencia para homepage
+    return render_template("homepage.html", form=form_login)
 
 #tela criar conta
 @app.route("/criarconta",  methods=["GET", "POST"])
-def criarconta():
-    formcriarconta = FormCriarConta()
-    return render_template('criarconta.html', form=formcriarconta)
+def criar_conta():
+    form_criarconta = FormCriarConta()
+    if form_criarconta.validate_on_submit():  #criar usuario
+        senha = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        usuario = Usuario(username=form_criarconta.username.data,
+                          senha=senha, email=form_criarconta.email.data)
+        #armazenar usuario no bd
+        database.session.add(usuario)
+        database.session.commit()
+        login_user(usuario, remember=True)
+        return redirect(url_for("perfil", usuario=usuario.username)) #ao criar conta redireciona para o perfil
+    return render_template('criarconta.html', form=form_criarconta)
 
 
 @app.route("/perfil/<usuario>")#cria uma pagina para cada usuario que acessar
